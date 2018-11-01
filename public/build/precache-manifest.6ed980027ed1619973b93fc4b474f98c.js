3d041d356959c65b68fb76fb20957739a78aa01d
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-01 12:05:21 +0530

    Added the build folder for rendering the app which was created in create-react-app

---
+++ public/build/precache-manifest.6ed980027ed1619973b93fc4b474f98c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eabe6eeb64312385adf8",
+    "url": "/static/js/main.eabe6eeb.chunk.js"
+  },
+  {
+    "revision": "33572fb4a4748bddf753",
+    "url": "/static/js/1.33572fb4.chunk.js"
+  },
+  {
+    "revision": "d88e686b363c9bb1068dd8a43b9a39c2",
+    "url": "/index.html"
+  }
+];
---
+++ public/build/scripts/script.js
@@ -0,0 +1,11 @@
+//To handle the modal window and Voice
+document.body.onload = playFunc
+function playFunc () {
+  responsiveVoice.speak("Hello Procrastinators, welcome to the secret diary of your dreams!", "UK English Male")
+}
+
+var closer = document.querySelector('.close')
+closer.addEventListener('click', () => {
+  var toBeClosed = document.querySelector('.modalWindow')
+  toBeClosed.style.display = 'none'
+})
